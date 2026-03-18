d1506538a97e2e715312fd8b67c9a49ea13e2bd8
asofimp <asofimp@gmail.com>
2026-03-17 20:43:01 -0600

    grpc

--- internal/model/suscription.go
+++ internal/model/suscription.go
@@ -6,7 +6,7 @@ type Suscription struct {
 	ID         uint      `json:"id" gorm:"primaryKey;autoIncrement;column:id_suscription"`
 	TenantID   uint      `json:"tenant_id" gorm:"column:tenant_id;not null;index"`
 	PlanID     uint      `json:"plan_id" gorm:"column:plan_id;not null;index"`
-	Plan       Plan      `json:"plan" gorm:"foreignKey:PlanID;references:ID"`
+	Plan       *Plan     `json:"plan" gorm:"foreignKey:PlanID;references:ID"`
 	AStartedAt time.Time `json:"astarted_at" gorm:"column:astarted_at;type:timestamptz"`
 	RenewAt    time.Time `json:"renew_at" gorm:"column:renew_at;type:timestamptz"`
 	EndAt      time.Time `json:"end_at" gorm:"column:end_at;type:timestamptz"`
